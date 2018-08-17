54f8df3f2710ca76638c0ed66aa8fb1d15fa34e7
Igor Bongartz <igorbongartz@gmail.com>
2018-08-17 14:09:00 +0200

    testproject2: Display amount of comics dynamically on top of the page

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+
 function fetchComics () {
-  var comics = ["https://imgs.xkcd.com/comics/equations.png", "https://imgs.xkcd.com/comics/home_organization.png", "https://imgs.xkcd.com/comics/scrabble.png", "https://imgs.xkcd.com/comics/trade_expert.png"];
+  var comics = [
+      "https://imgs.xkcd.com/comics/equations.png",
+      "https://imgs.xkcd.com/comics/home_organization.png",
+      "https://imgs.xkcd.com/comics/scrabble.png",
+      "https://imgs.xkcd.com/comics/trade_expert.png"
+  ];
   var comicsList = document.getElementById('comicsList');
 
   comicsList.innerHTML = '';
@@ -8,9 +20,19 @@ function fetchComics () {
     var url = comics[i];
 
     comicsList.innerHTML +=
-        '<br>' +
+        '<div class="row">' +
+        '<div class="col-md-4">' +
         '<a href="' + url + '">'+
-        '<img src="' + url + '" alt="test"/>' +
-        '</a>';
+        '<img src="' + url + '" alt="test" />' +
+        '</a>' +
+        '</div>' +
+        '</div>';
   }
+
+  var tcl = document.getElementById('totalcomicsloaded');
+
+  tcl.innerHTML = 'Total comics loaded ' + comics.length;
+
+  var json_obj = JSON.parse(Get("https://xkcd.com/info.0.json"));
+  alert("test");
 }
